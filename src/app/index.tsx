faa559889bac3ca8ea7fbe55590c042743e303e1
SongHuyITITIU19134 <80945626+SongHuyITITIU19134@users.noreply.github.com>
2024-03-28 16:10:26 +0700

    Create Food List Item

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,40 @@
+import { AntDesign } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+const FoodListItem = ({ item }) => {
+  return (
+    <View style={{
+      backgroundColor: "gainsboro",
+      padding: 10,
+      borderRadius: 5,
+
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    }}>
+      <View style={{ flex: 1, gap: 5, }}>
+        <Text style={{ fontWeight: 'bold', fontSize: 16 }}> {item.lable}</Text>
+        <Text style={{ color: 'dimgray' }}>{item.cal} cal, {item.brand}</Text>
+      </View>
+      <View style={{ marginRight: 10, }}>
+        <TouchableOpacity>
+          <AntDesign name="pluscircleo" size={24} color="royalblue" />
+        </TouchableOpacity>
+
+      </View>
+    </View>
+  )
+
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Huy Nguyen</Text>
+      {/* Food Item View */}
+      <FoodListItem item={{ lable: 'Pizza', cal: 50, brand: "Binh Phuoc" }} />
+      <FoodListItem item={{ lable: 'Chicken', cal: 200, brand: "USA" }} />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +44,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    padding: 10,
     justifyContent: 'center',
   },
 });
